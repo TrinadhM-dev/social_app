f4882cbd338f05aa47c6ad9e39b22f78e306c0c4
Trinadh Machanavazzala <trinadhm469@gmail.com>
2024-10-14 21:35:20 -0500

    Shared Component with customized box-generator

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './feed.css';
+import Share from '../shared-components/Share';
 function Feed() {
   return (
     <div className='feed'>
       <div className="feedWrapper">
-        
+        <Share/>
       </div>
     </div>
   )
