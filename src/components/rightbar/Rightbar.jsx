ec14e691cb900637deda3ee518ba51c34ac437f0
Trinadh Machanavazzala <trinadhm469@gmail.com>
2024-10-15 00:35:35 -0500

    Right bar Component

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,86 @@ import './rightbar.css';
 export default function Rightbar() {
   return (
     <div className='rightbar'>
-      rightbar
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className='birthdayImg' src="/assets/gift.jpg" alt="" />
+          <span className='birthdayText'>
+            <b>Gopi Lokindi</b> and three other friends.</span>
+        </div>
+        <img src="/assets/advertising.png" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className='rightbarProfileImg' src="assets/person/rand04.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Rasool Shaik Khaja Nayab</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className='rightbarProfileImg' src="assets/person/rand05.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Pavan Kumar Vanamala</span>
+          </li>
+
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className='rightbarProfileImg' src="assets/person/rand01.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Gopi Lokindi</span>
+          </li>
+
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className='rightbarProfileImg' src="assets/person/rand02.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Karthik Ravuri</span>
+          </li>
+
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className='rightbarProfileImg' src="assets/person/rand03.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Prasanna Kumar</span>
+          </li>
+
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className='rightbarProfileImg' src="assets/person/rand04.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Adithya Muppala</span>
+          </li>
+
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className='rightbarProfileImg' src="assets/person/rand01.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Praveen Kumar Chidipothu</span>
+          </li>
+
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className='rightbarProfileImg' src="assets/person/rand05.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Bharat Kondepati</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
