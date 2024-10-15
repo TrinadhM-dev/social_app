3a2acdf93ef22dcec23e760eeb5525052679d0a6
Trinadh Machanavazzala <trinadhm469@gmail.com>
2024-10-14 22:30:59 -0500

    Feed Component and updated images

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './feed.css';
 import Share from '../shared-components/Share';
+import Post from '../post/Post';
 function Feed() {
   return (
     <div className='feed'>
       <div className="feedWrapper">
         <Share/>
       </div>
+      <Post/>
     </div>
   )
 }
---
+++ src/components/post/Post.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './post.css';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+const Post = () => {
+  return (
+    <div className='post'>
+      <div className="postWrapper">
+        
+        
+        <div className="postTop">
+              <div className="postTopLeft">
+                 <img className='postProfileImg' src="/assets/person/rand01.jpg" alt='' />
+                 <span className="postUsername">Tri Mach </span>
+                 <span className="postDate">5 mins Ago</span>
+             </div>
+        
+            <div className="postTopRight">
+                <MoreVertIcon/>
+            </div>
+        </div>
+
+
+        <div className="postCenter">
+        <span className="postText">Hey! It's my First Post :)</span>
+        
+        <img className='postImg' src="/assets/post/love-bird.post.jpg" alt="" />
+
+        </div>
+
+        <div className="postBottom">
+            <div className="postBottomLeft">
+                <img className='likeIcon' src="/assets/likes.png" alt="" />
+                <img className='heartIcon' src="/assets/heart.png" alt="" />
+                <span className="postLikeCounter">32 people likes your post</span>
+            </div>
+            <div className="postBottomRight">
+               <span className="postCommentText">9 Comments</span>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Post
